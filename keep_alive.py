ca381108ac7ae7d957775684fb47ee4ad20edecf
Hayden Housen <hayden@haydenhousen.com>
2021-10-12 21:04:19 -0400

    Get uptime url from environment correctly

--- keep_alive.py
+++ keep_alive.py
@@ -3,7 +3,6 @@ import requests
 from flask import Flask
 from threading import Thread, Timer
 
-UPTIME_CHECK_URL = None
 app = Flask(__name__)
 
 
@@ -19,14 +18,13 @@ def run():
 def log_uptime():
     Timer(60, log_uptime).start()
     try:
-        requests.get(UPTIME_CHECK_URL, timeout=10)
+        requests.get(os.environ["UPTIME_CHECK_URL"], timeout=10)
     except requests.RequestException as e:
         print("Ping failed: %s" % e)
 
 
 def keep_alive():
-    if "UPTIME_CHECK_URL" in os.environ:
-        UPTIME_CHECK_URL = os.environ["UPTIME_CHECK_URL"]
+    if "UPTIME_CHECK_URL" in os.environ.keys():
         log_uptime()
     t = Thread(target=run)
     t.start()
